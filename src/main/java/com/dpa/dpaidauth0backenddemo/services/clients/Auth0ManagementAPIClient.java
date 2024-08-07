ea9fe7141ad2136a90e6957c8e408b4ff871e063
Arif Pehlivan <m.arifpehlivan@gmail.com>
2024-08-07 14:58:15 +0200

    Add redirect uri

--- src/main/java/com/dpa/dpaidauth0backenddemo/services/clients/Auth0ManagementAPIClient.java
+++ src/main/java/com/dpa/dpaidauth0backenddemo/services/clients/Auth0ManagementAPIClient.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
-import java.util.Map;
+import java.util.List;
 
 @Component
 public class Auth0ManagementAPIClient {
@@ -32,21 +34,19 @@ public class Auth0ManagementAPIClient {
     }
 
     public Auth0ValidateCodeResponseDTO validateCode(String code) {
-        try {
-            return createClient()
-                    .post()
-                    .uri("/oauth/token")
-                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                    .body(mapper.writeValueAsString(Map.of(
-                            "client_id", clientId,
-                            "client_secret", clientSecret,
-                            "grant_type", "authorization_code",
-                            "code", code)))
-                    .retrieve()
-                    .body(Auth0ValidateCodeResponseDTO.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+      MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+      map.put("code", List.of(code));
+      map.put("client_id", List.of(clientId));
+      map.put("client_secret", List.of(clientSecret));
+      map.put("grant_type", List.of("authorization_code"));
+      map.put("redirect_uri", List.of("https://backend-demo.dpa-id.de"));
+      return createClient()
+              .post()
+              .uri("/oauth/token")
+              .body(map)
+              .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+              .retrieve()
+              .body(Auth0ValidateCodeResponseDTO.class);
     }
 
 
